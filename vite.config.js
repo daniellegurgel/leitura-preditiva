a794a9707d13bde83521f3f28b1b8314ea6d9814
daniellegurgel <gurgel.danielle@gmail.com>
2025-07-06 13:09:31 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['recharts'] // Isso diz ao Rollup para não incluir 'recharts' no bundle
+    }
+  }
+});
