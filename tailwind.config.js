33373ff2eb0738b8fb5b36ec3d4858f03737a3bc
daniellegurgel <gurgel.danielle@gmail.com>
2025-07-06 14:55:33 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Isso escaneia todos os seus arquivos JS/TS/JSX/TSX dentro de src
+    // Adicione a linha abaixo se o seu CSS customizado tiver classes que não estão em JS/JSX
+    "./src/*.css", // Isso garante que o Tailwind escaneie seu index.css ou outros arquivos CSS dentro de src
   ],
   theme: {
     extend: {},
